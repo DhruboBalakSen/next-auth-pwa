3dd940a3008a9f1c794be74128b5dba91f107d50
Dhrubo Balak Sen <dhrubosen206@gmail.com>
2025-07-30 16:41:02 +0530

    Changed build command

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,9 +1,9 @@
-import type { Metadata, Viewport } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
-import { Toaster } from "react-hot-toast";
-import { Suspense } from "react";
 import Header from "@/components/Header";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import { Suspense } from "react";
+import { Toaster } from "react-hot-toast";
+import "../globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
